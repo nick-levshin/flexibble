f8fff3b86d33c19e519d16f41dfdce42829dba9a
Nick Levshin <nikita.levshin2016@gmail.com>
2023-09-21 22:37:19 +0300

    feat: add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import { FC } from 'react';
 interface homePageProps {}
 
 const HomePage: FC<homePageProps> = ({}) => {
-  return <div>Home</div>;
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1> <h1>Posts</h1> <h1>LoadMore</h1>
+    </section>
+  );
 };
 
 export default HomePage;
