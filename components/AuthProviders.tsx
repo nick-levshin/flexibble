75189541aca4897f8f3ebf511ab722a3fe5cde82
Nick Levshin <nikita.levshin2016@gmail.com>
2023-09-21 22:46:55 +0300

    feat: add navbar component

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface AuthProvidersProps {}
+
+const AuthProviders: FC<AuthProvidersProps> = ({}) => {
+  return <div>AuthProviders</div>;
+};
+
+export default AuthProviders;
